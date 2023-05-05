57c7b6c128f3c2d63b4a874c7fbe5ed1312b9e19
MathieuRdn16 <rakotondradano.mathieu@gmail.com>
2023-05-05 21:22:44 +0300

    b1

--- back-office/app/Providers/AppServiceProvider.php
+++ back-office/app/Providers/AppServiceProvider.php
@@ -3,23 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
-
+use \URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
      */
     public function boot(): void
     {
+        //
         if($this->app->environment('production')){
             \URL::forceScheme('https');
         }
