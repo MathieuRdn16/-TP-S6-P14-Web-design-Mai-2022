2acdc5ae2c1b00dcf1ead613f14232a30e7b6d7c
MathieuRdn16 <rakotondradano.mathieu@gmail.com>
2023-05-07 12:35:39 +0300

    Update ArticleController.php

--- front-office/app/Http/Controllers/ArticleController.php
+++ front-office/app/Http/Controllers/ArticleController.php
@@ -2,20 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Article;
-
+use Illuminate\Support\Facades\Cache;
 class ArticleController extends Controller
 {
 
     public function liste(){
-		return view('listeArticle',['listes'=>Article::all()]);
+		$list=Cache::remember('listes',300,function (){
+            return Article::all();
+        });
+        $response = response()->view('listeArticle',['listes'=>$list]);
+        $response->header('Cache-Control','max-age=3600 , public ');
+        return $response;
+        //return view('listeArticle',['listes'=>Article::all()]);
 	}
 
     public function fiche($id){
-        return view('ficheArticle',[
-            'article'=>Article::find($id)
-        ]);
+        $find=Cache::remember('article',120,function () use($id){
+            return Article::find($id);
+        });
+        $response = response()->view('ficheArticle',['article'=>$find]);
+        $response->header('Cache-Control','max-age=3600 , public ');
+        return $response;
+        //return view('ficheArticle',[
+        //    'article'=>Article::find($id)
+        //]);
     }
 
 }
